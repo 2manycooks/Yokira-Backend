1109d3bb69b4fbd4afd1f41807311d3b7295fc46
Gabriel Gangoso <gabriel.gangoso@gmail.com>
2021-03-03 18:32:28 -0800

    testing with django_heroku import

--- yokira/settings.py
+++ yokira/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -148,4 +147,7 @@ STATICFILES_DIRS = (
 )
 
 DISABLE_COLLECTSTATIC=1
+
+# Configure Django App for Heroku.
+import django_heroku
 django_heroku.settings(locals())
